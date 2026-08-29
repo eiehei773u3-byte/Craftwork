0845de5e2ca62aa996bbd914c713cfcdc853806e
eiehei773u3-byte <eiehei773u3@gmail.com>
2026-08-29 08:28:36 +0000

    Update project files

--- src/core/root.cpp
+++ src/core/root.cpp
@@ -1,10 +1,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
+
     SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
@@ -14,9 +14,12 @@ int main(int argc, char* argv[]) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = false;
         }
+
+        // Очистка экрана и отображение кадра
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
     }
-
-
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
